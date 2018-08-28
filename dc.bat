1228f8e5b249b6ed6f2c57776f7b294e09ca5ef3
Prabhakar Thopa <prabhakar.thopa@unisys.com>
2018-08-28 11:17:30 -0400

    updated jenkinsfile

--- dc.bat
+++ dc.bat
@@ -1,8 +1,2 @@
-@echo off
-docker-machine start
-docker-machine ip > hostname.txt
-set /p REN_RT_EXTERNAL_IP=<hostname.txt
-del hostname.txt
-echo Host-ip: %REN_RT_EXTERNAL_IP%
-@FOR /f "tokens=*" %%i IN ('docker-machine env') DO %%i
+docker-machine env default
 call docker-compose up
